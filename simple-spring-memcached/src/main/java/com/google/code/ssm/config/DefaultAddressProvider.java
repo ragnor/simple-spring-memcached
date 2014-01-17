8e64ebd2f27f683630180506de2f0ed1441b1a6e
ragnor <ragnor84@gmail.com>
2014-01-17 15:55:08 +0000

    Update copyrights

--- simple-spring-memcached/src/main/java/com/google/code/ssm/config/DefaultAddressProvider.java
+++ simple-spring-memcached/src/main/java/com/google/code/ssm/config/DefaultAddressProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013 Nelson Carpentier, Jakub Białek
+ * Copyright (c) 2012-2014 Nelson Carpentier, Jakub Białek
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
