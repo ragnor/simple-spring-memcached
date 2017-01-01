36ef308f47636bae4ce4fe5dddfdbf8b31105453
ragnor <ragnor84@gmail.com>
2017-01-01 21:00:30 +0100

    Update copyright

--- aws-elasticache-provider/src/test/java/com/google/code/ssm/providers/elasticache/CachedObjectWrapperTest.java
+++ aws-elasticache-provider/src/test/java/com/google/code/ssm/providers/elasticache/CachedObjectWrapperTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016 Jakub Białek
+/* Copyright (c) 2014-2017 Jakub Białek
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
